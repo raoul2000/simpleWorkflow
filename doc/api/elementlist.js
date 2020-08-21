3dcdd69000255be593a27d60e0da18f04e2113ec
raoul2000 <manu34@free.Fr>
2020-08-21 20:08:38 +0200

    update doc and add api doc

---
+++ doc/api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","SWActiveRecord"],["c","SWActiveRecordBehavior"],["c","SWComponent"],["c","SWEvent"],["c","SWException"],["c","SWHelper"],["c","SWNode"],["c","SWPhpWorkflowSource"],["c","SWValidator"],["c","SWWorkflowSource"],["c","SWyEdConverter"],["c","SWyEdConverterDOM"]];
